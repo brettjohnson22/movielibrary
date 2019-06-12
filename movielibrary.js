5133d25a69857ee5a93f8f35193f6fb9afe4b7bb
Brett Johnson <br.onson@outlook.com>
2019-06-12 10:02:03 -0500

    Get button displays table data on html page

---
+++ movielibrary.js
@@ -0,0 +1,22 @@
+
+
+
+function get(){
+	$(function(){
+		$.ajax({
+			type: 'GET',
+			url: 'https://localhost:44319/api/values',
+			datatype: 'json',
+			success: function(data){
+				//$('#allmovies').html('<tr>data[0]</tr>');
+				$('#allmovies').empty();
+				$('#allmovies').append('<tr><td>Title</<td><td>Genre</td><td>Director</td><tr>')
+				$.each(data, function (i, item){
+					$('#allmovies').append('<tr><td>' + data[i].Title + '</td><td>' + data[i].Genre + '</td><td>' + data[i].DirectorName + '</td></tr>');
+				});
+			}
+		});
+	});
+};
+
+//
